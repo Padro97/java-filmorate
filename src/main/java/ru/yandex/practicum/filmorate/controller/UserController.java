b73dc062205c430495e567f93781406686448097
Nikita <np12231223@yandex.ru>
2024-02-17 13:19:10 +0300

    Исправил UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
@@ -22,38 +23,41 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody User user) {
+        Gson gson = new Gson();
+        String jsonResponse = gson.toJson(user);
         try {
             if (user.getEmail() == null || !user.getEmail().contains("@")) {
                 log.error("400 - Invalid email");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid email");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonResponse);
             }
 
             if (user.getId() < 1) {
                 log.error("400 - Invalid user ID");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid user ID");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonResponse);
             }
 
             if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
                 log.error("400 - Invalid login");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid login");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonResponse);
+
             }
 
             if (user.getName() == null || user.getName().isEmpty()) {
                 log.error("400 - Invalid name");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid name");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonResponse);
             }
 
             if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
                 log.error("400 - Invalid birthday");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid birthday");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonResponse);
             }
 
             users.add(user);
             log.info("200 - User created successfully");
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(jsonResponse);
         } catch (Exception e) {
             log.error("500 - Internal Server Error", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(jsonResponse);
         }
     }
 
