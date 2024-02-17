9a450e5a7cf08d00d2a7ef3a31b63248bfd60211
Nikita <np12231223@yandex.ru>
2024-02-17 11:03:57 +0300

    Исправил User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,14 +6,17 @@ import java.time.LocalDate;
 @Data
 public class User {
     private static int nextId = 1;
+
     private int id;
     private String email;
     private String login;
     private String name;
     private LocalDate birthday;
+
     public User() {
         this.id = nextId++;
     }
 }
+
 
 
